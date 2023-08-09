89171794a7e5b48df0dc0c3bf2374856a688df2e
kunal <kunalaage21@gmail.com>
2023-08-09 12:02:34 +0530

    feature: products

--- src/pages/ProductList/ProductList.tsx
+++ src/pages/ProductList/ProductList.tsx
@@ -117,18 +117,6 @@ const ProductList = () => {
         </div>
         <div className="row">
           <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-            <div className="widget widget-chart-one my-4">
-              {/* <div className="widget-heading">
-                      <h5>Candidates</h5>
-                  </div> */}
-
-              <div className="widget-content">
-                {/* <AdminLineCharts data={employerMonthChart} /> */}
-              </div>
-            </div>
-          </div>
-
-          <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
             <div className="widget widget-table-two">
               <div className="widget-heading">
                 <h5>Product List</h5>
